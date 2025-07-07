8bdba68c6bfe7956529851e27998e6492234b74f
AgungBahtiarr <agungbahtiar050@gmail.com>
2025-07-07 15:42:14 +0700

    use svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +12,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   adapter: node({ mode: "standalone" }),
+  integrations: [svelte()],
 });
